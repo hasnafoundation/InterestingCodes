743f8f6e0e56a2ea27bb68fee3665a9dedbc2daf
CFV <vescovocarlofederico@gmail.com>
2019-10-16 19:34:36 +0200

    Fast fibonacci implemented (Python)

--- Python/Fibonacci/fibonacci.py
+++ Python/Fibonacci/fibonacci.py
@@ -1,8 +1,16 @@
-def fibonacci(n):
+def fib_recursive(n):
     if (n <= 1):
         return n
     else: 
-        return fibonacci(n-1) + fibonacci(n-2)
+        return fib_recursive(n-1) + fib_recursive(n-2)
+
+def fib(n):
+    if n in (0, 1):
+        return 1
+    if n & 1:  # if n is odd, it's faster than checking with modulo
+        return fib((n+1)//2 - 1) * (2*fib((n+1)//2) - fib((n+1)//2 - 1))
+    a, b = fib(n//2 - 1), fib(n//2)
+    return a**2 + b**2
 
 try:
     x = int(input("Insert an integer: "))
@@ -10,4 +18,4 @@ except ValueError:
     print("Not an integer!")
     exit(1)
 
-print(str(x) + "° Fibonacci number = " + str(fibonacci(x)))
+print(str(x) + "° Fibonacci number = " + str(fib(x)))
